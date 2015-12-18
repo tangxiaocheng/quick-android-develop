5a5333ac03fa66661bc11b0bace8da03eba05def
tangdengcheng <tangdengcheng@gmail.com>
2015-12-18 10:55:24 +0800

    test

--- src/android/build/code/BuildCode.java
+++ src/android/build/code/BuildCode.java
@@ -3,6 +3,16 @@ package android.build.code;
 public class BuildCode {
 	public static void main(String[] args) {
 		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+		System.out.println("hello github ..");
+
 	}
 
 }

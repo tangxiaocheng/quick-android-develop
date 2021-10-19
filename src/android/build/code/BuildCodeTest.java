ff1aa4718d6a7f7dc26544fba1174bd976950e9b
Randy Tang <tangdengcheng@gmail.com>
2021-10-19 10:02:58 -0700

    Add generation for cpp

--- src/android/build/code/BuildCodeTest.java
+++ src/android/build/code/BuildCodeTest.java
@@ -126,4 +126,220 @@ public class BuildCodeTest {
                         xmlJson, false);
         makeAndroidXml2Code.makeOnCreateCode();
     }
+
+
+    @Test
+    public void generate_activity_phone_number() {
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+//        cpp_title_bar_all.xml
+
+
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_phone_number",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
+
+    @Test
+    public void generate_activity_phone_number_exist() {
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_phone_number_exist",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
+
+    //activity_cpp_pin_education.xml
+    @Test
+    public void generate_activity_cpp_pin_education() {
+
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_cpp_pin_education",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
+
+    //activity_cpp_create_pin.xml
+    @Test
+    public void generate_activity_cpp_create_pin() {
+
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_cpp_create_pin",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
+
+    //activity_safe_word_education.xml
+    @Test
+    public void generate_activity_safe_word_education() {
+
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_safe_word_education",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
+
+    //activity_set_up_safe_word.xml
+    @Test
+    public void generate_activity_set_up_safe_word() {
+
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_set_up_safe_word",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
+
+    //activity_cpp_verification_code.xml
+    @Test
+    public void generate_activity_cpp_verification_code() {
+
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_cpp_verification_code",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
+
+    //activity_cpp_set_home_name.xml
+    @Test
+    public void generate_activity_cpp_set_home_name() {
+
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_cpp_set_home_name",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
+
+    //activity_cam_plus_pro_setup.xml
+    @Test
+    public void generate_activity_cam_plus_pro_setup() {
+
+        System.out.println("run... ");
+        System.out.println(Viewstant.PROJECT_FILE_PATH);
+
+        String xmlJson = "";
+        String androidAppPath = "/Users/randytang/WyzeAndroidCppTask/WyzePluginHmsAndroid";
+        String targetActivityPackageName = "com.wyze.cpp.setup";
+        String appPackageName = "com.wyze.hms";
+
+        NewActivityByXml makeAndroidXml2Code =
+                new NewActivityByXml(
+                        androidAppPath,
+                        true,
+                        true,
+                        targetActivityPackageName,
+                        "activity_cam_plus_pro_setup",
+                        appPackageName,
+                        xmlJson, false);
+        makeAndroidXml2Code.makeOnCreateCode();
+    }
 }
